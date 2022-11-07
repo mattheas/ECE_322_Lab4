f907424af8a67c24aa83217a0390a00633e6b6e5
mattheas <mattheas@ualberta.ca>
2022-11-06 23:34:39 -0700

    getter method unit test completed, setter unit test started

--- tests/TestModuleB.py
+++ tests/TestModuleB.py
@@ -1,7 +1,9 @@
 import unittest
 from modules.ModuleB import ModuleB
-# from unittest.mock import Mock
+from unittest.mock import Mock
 
+# no need to test FileNotFOundError in Module B as this would require student to intentionally change
+# a files permission to be not readable by current user
 
 class TestB(unittest.TestCase):
 
@@ -9,10 +11,28 @@ class TestB(unittest.TestCase):
         print("ello govna")
 
     def test_setter_f(self):
-        print("ello govna")
-
+        # create instance of Module B w/ mock Mod. F obj
+        mock_ModuleF = Mock()
+        test_ModuleB = ModuleB(mock_ModuleF)
+        
+        # assert that getter method retrieves mock Mod. F obj
+        self.assertEqual(test_ModuleB.f, mock_ModuleF)
+        
+        # use setter method
+        new_mock_ModuleF = Mock()
+        test_ModuleB.f(new_mock_ModuleF)
+        
+        # assert method updated attribute
+        self.assertEqual(test_ModuleB.f, new_mock_ModuleF)
+        
+        
     def test_getter_f(self):
-        print("ello govna")
+        # create instance of Module B w/ mock Mod. F obj
+        mock_ModuleF = Mock()
+        test_ModuleB = ModuleB(mock_ModuleF)
+        
+        # assert that getter method retrieves mock Mod. F obj
+        self.assertEqual(test_ModuleB.f, mock_ModuleF)
 
 
 if __name__ == '__main__':
