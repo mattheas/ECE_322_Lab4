bd107b4041c44bf19b61853704781ca3717c829b
mattheas <mattheas@ualberta.ca>
2022-11-11 13:15:35 -0700

    setUp() method added to increase readability, tests for parseLoad/Delete updated to provide 100% statement coverage

--- tests/TestModuleA.py
+++ tests/TestModuleA.py
@@ -4,73 +4,68 @@ from unittest.mock import Mock
 
 class TestA(unittest.TestCase):
 
-    def test_parseDelete_method(self):
+    def setUp(self):
         # create instance of Module A w/ mock obj's for parameters
-        mock_ModuleB = Mock()
-        mock_ModuleC = Mock()
-        mock_ModuleD = Mock()
-        mock_ModuleE = Mock()
-        test_ModuleA = ModuleA(mock_ModuleB, mock_ModuleC, mock_ModuleD, mock_ModuleE)
+        self.mock_ModuleB = Mock()
+        self.mock_ModuleC = Mock()
+        self.mock_ModuleD = Mock()
+        self.mock_ModuleE = Mock()
+        self.test_ModuleA = ModuleA(self.mock_ModuleB, self.mock_ModuleC, self.mock_ModuleD, self.mock_ModuleE)
+
+
+    def test_parseDelete_method(self):
         
         test_filename = "test_file_ModuleA.txt"
         test_data = []
-        test_ModuleA.filename = test_filename
-        test_ModuleA.data = test_data
-        
-        # assertion statement, method returns true because d is a mocked obj, so the method call just returns a 
-        # mock obj, meaning self._data != None
+        self.test_ModuleA.filename = test_filename
+        self.test_ModuleA.data = test_data
         index_to_del = 2
-        self.assertTrue(test_ModuleA.parseDelete(index_to_del))
+        
+        # mock return value of deleteData method to return empty list
+        self.mock_ModuleD.deleteData.return_value = []
+        self.assertTrue(self.test_ModuleA.parseDelete(index_to_del))
+        
+        # mock return value of deleteData method to return None
+        self.mock_ModuleD.deleteData.return_value = None
+        self.assertFalse(self.test_ModuleA.parseDelete(index_to_del))
         
         
     def test_displayHelp_method(self):
-        # create instance of Module A w/ mock obj's for parameters
-        mock_ModuleB = Mock()
-        mock_ModuleC = Mock()
-        mock_ModuleD = Mock()
-        mock_ModuleE = Mock()
-        test_ModuleA = ModuleA(mock_ModuleB, mock_ModuleC, mock_ModuleD, mock_ModuleE)
+        
+        # mock the method??
         
         # assertion
-        self.assertTrue(test_ModuleA.displayHelp())
+        self.assertTrue(self.test_ModuleA.displayHelp())
         
         
     def test_parseLoad_method(self):
-        # create instance of Module A w/ mock obj's for parameters
-        mock_ModuleB = Mock()
-        mock_ModuleC = Mock()
-        mock_ModuleD = Mock()
-        mock_ModuleE = Mock()
-        test_ModuleA = ModuleA(mock_ModuleB, mock_ModuleC, mock_ModuleD, mock_ModuleE)
         
         test_filename = "test_file_ModuleA.txt"
         test_data = []
-        test_ModuleA.data = test_data
+        self.test_ModuleA.data = test_data
         
-        # assertion statement, method returns true because b is a mocked obj, so the method call just returns a 
-        # mock obj, meaning self._data != None
-        self.assertTrue(test_ModuleA.parseLoad(test_filename))
+        # mock return value of loadFile method to return empty list
+        self.mock_ModuleB.loadFile.return_value = []
+        self.assertTrue(self.test_ModuleA.parseLoad(test_filename))
+        
+        # mock return value of loadFile method to return None
+        self.mock_ModuleB.loadFile.return_value = None
+        self.assertFalse(self.test_ModuleA.parseLoad(test_filename))
         
         
     def test_parseAdd_method(self):
-        # create instance of Module A w/ mock obj's for parameters
-        mock_ModuleB = Mock()
-        mock_ModuleC = Mock()
-        mock_ModuleD = Mock()
-        mock_ModuleE = Mock()
-        test_ModuleA = ModuleA(mock_ModuleB, mock_ModuleC, mock_ModuleD, mock_ModuleE)
         
         test_filename = "test_file_ModuleA.txt"
         test_data = []
-        test_ModuleA.filename = test_filename
-        test_ModuleA.data = test_data
+        self.test_ModuleA.filename = test_filename
+        self.test_ModuleA.data = test_data
         
         test_name = "Bobby"
         test_number = "42349"
         
         # assertion statement, method returns true because d is a mocked obj, so the method call just returns a 
         # mock obj, meaning self._data != None
-        self.assertTrue(test_ModuleA.parseAdd(test_name, test_number))
+        self.assertTrue(self.test_ModuleA.parseAdd(test_name, test_number))
         
         
 
