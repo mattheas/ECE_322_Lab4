1da7dd628b1f7a6294d18773bcef2d69f10d6cc2
mattheas <mattheas@ualberta.ca>
2022-11-12 17:05:32 -0700

    Half of run method tested

--- tests/TestModuleA.py
+++ tests/TestModuleA.py
@@ -14,31 +14,28 @@ class TestA(unittest.TestCase):
         self.mock_ModuleD = Mock()
         self.mock_ModuleE = Mock()
         self.test_ModuleA = ModuleA(self.mock_ModuleB, self.mock_ModuleC, self.mock_ModuleD, self.mock_ModuleE)
-        
         self.test_filename = "test_file_ModuleA.txt"
-
+        self.test_data = []
 
     def test_parseDelete_method(self):
-        
-        test_data = []
+        self.test_data = []
         self.test_ModuleA.filename = self.test_filename
-        self.test_ModuleA.data = test_data
+        self.test_ModuleA.data = self.test_data
         index_to_del = 2
         
         # mock return value of deleteData method to return empty list
         self.mock_ModuleD.deleteData.return_value = []
         self.assertTrue(self.test_ModuleA.parseDelete(index_to_del))
-        self.mock_ModuleD.deleteData.assert_called_with(test_data, index_to_del, self.test_filename)
+        self.mock_ModuleD.deleteData.assert_called_with(self.test_data, index_to_del, self.test_filename)
         
         # mock return value of deleteData method to return None
         self.mock_ModuleD.deleteData.return_value = None
         self.assertFalse(self.test_ModuleA.parseDelete(index_to_del))
-        self.mock_ModuleD.deleteData.assert_called_with(test_data, index_to_del, self.test_filename)
+        self.mock_ModuleD.deleteData.assert_called_with(self.test_data, index_to_del, self.test_filename)
         
         
     @patch.object(ModuleA, "displayHelp")
     def test_displayHelp_method(self, mock_displayHelp):
-        
         # call to mock method
         mock_displayHelp()
         
@@ -48,9 +45,8 @@ class TestA(unittest.TestCase):
         
         
     def test_parseLoad_method(self):
-        
-        test_data = []
-        self.test_ModuleA.data = test_data
+        self.test_data = []
+        self.test_ModuleA.data = self.test_data
         
         # mock return value of loadFile method to return empty list
         self.mock_ModuleB.loadFile.return_value = []
@@ -62,10 +58,9 @@ class TestA(unittest.TestCase):
         
         
     def test_parseAdd_method(self):
-        
-        test_data = []
+        self.test_data = []
         self.test_ModuleA.filename = self.test_filename
-        self.test_ModuleA.data = test_data
+        self.test_ModuleA.data = self.test_data
         
         test_name = "Bobby"
         test_number = "42349"
@@ -80,7 +75,6 @@ class TestA(unittest.TestCase):
         
         
     def test_runSort_method(self):
-    
         # mock return value of sortData method to return empty list
         self.mock_ModuleC.sortData.return_value = []
         self.assertTrue(self.test_ModuleA.runSort())
@@ -96,7 +90,7 @@ class TestA(unittest.TestCase):
         test_name1 = "Hanny"
         test_num = "87635"
         test_num1 = "96453"
-        test_data = [Entry(test_name,test_num), Entry(test_name1,test_num1)]
+        self.test_data = [Entry(test_name,test_num), Entry(test_name1,test_num1)]
         
         test_new_name = "Not Hanny"
         test_new_number = "123693"
@@ -121,19 +115,41 @@ class TestA(unittest.TestCase):
     
     
     @patch.object(ModuleA, "run")
-    def test_run_NO_command_given_method(self, mock_run):
-        
-        # call to mock method
+    def test_run_NO_and_help_command_given_method(self, mock_run):
+        # call to mock method w/ no command
         mock_run()
         
         # assertion statements
         mock_run.assert_called_with()
         mock_run.assert_called_once()
         
+        # call to mock method w/ help command
+        mock_run("help")
+        mock_run.assert_called_with("help")      
+                
+                
+    def test_run_add_command_given_method(self):
+        self.test_ModuleA._data = [Entry("hello","54")]
+        self.test_ModuleA.run("add")
+        self.mock_ModuleD.insertData.assert_not_called() # IndexError so no method call
         
-    def test_run_command_given_method(self):
-        print("test")
-    
+        self.test_ModuleA._data = None
+        self.test_ModuleA.run("add")
+        self.mock_ModuleD.insertData.assert_not_called() # self.data == None so no method call
+        
+        self.test_ModuleA._data = []
+        self.test_ModuleA._filename = self.test_filename
+        self.test_ModuleA.run("add", "test_name_to_add", "76585")
+        self.mock_ModuleD.insertData.assert_called_with([], "test_name_to_add", "76585", self.test_filename)          
+   
+
+    def test_run_load_command_given_method(self):
+        # test load command
+        self.test_ModuleA.run("load")
+        self.mock_ModuleB.loadFile.assert_not_called()
+        self.test_ModuleA.run("load", self.test_filename)
+        self.mock_ModuleB.loadFile.assert_called_once_with(self.test_filename)
+        
         
 if __name__ == '__main__':
     unittest.main()
